5596a8d229d5bbd3499ac80918a00f8c8cdd79a1
mravicole <mravicole@balanz.com>
2021-01-09 16:58:41 -0300

    initial commit

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+const path = require('path');
+
+const expressStatic = express.static(path.resolve(__dirname, "public"));
+app.use(expressStatic);
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/home.html'));
+});
+
+app.listen(3000, () => {});
