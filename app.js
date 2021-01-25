a8b6f48006fd7c916492a86645d275fcefc58090
mravicole <mravicole@balanz.com>
2021-01-24 21:18:56 -0300

    agregado de register y login

--- app.js
+++ app.js
@@ -10,6 +10,14 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './views/home.html'));
 });
 
+app.get('/register', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/register.html'));
+});
+
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/login.html'));
+});
+
 app.listen(3000, () => {
     console.log("El puerto 3000 está encendido");
 });
